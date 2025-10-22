dbfcaf5823d17aee859af664411a89dc7977e8a3
Raymond Chong <mathsgod@yahoo.com>
2025-10-22 16:38:39 +0800

    ✨ feat(Adapter): 設定 `utf8mb4` 字符集
    * 在創建適配器實例時，若字符集為 `utf8mb4`，則執行 SQL 查詢以設置字符集和排序規則。
    * 確保數據庫連接使用正確的字符集，以支持更廣泛的字符範圍。

--- src/Adapter.php
+++ src/Adapter.php
@@ -67,6 +67,11 @@ class Adapter extends \Laminas\Db\Adapter\Adapter
             null,
             $profiler
         );
+
+        if ($charset == "utf8mb4") {
+            self::$instance->query("SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'")->execute();
+        }
+
         return self::$instance;
     }
 
